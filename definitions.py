af7d61ac22e6530b3b5d722808d622c3048eb0b6
David Risch <acc_mid@rischnet.de>
2020-06-06 17:33:15 +0200

    Changed size of figures

--- definitions.py
+++ definitions.py
@@ -4,7 +4,7 @@ color_histogram_border = "#000f"
 color_histogram_ideal = "#d46300" + "ff"
 color_dot = "#000f"
 
-plot_width = 7  # in inches
+plot_width = 5  # in inches
 plot_aspect_ratio = 0.75
 plot_height = plot_aspect_ratio * plot_width
 plot_dpi = 300
