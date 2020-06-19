e5859808feebf67c21f627f3ab3901afeb15be26
David Risch <acc_mid@rischnet.de>
2020-06-19 09:41:03 +0200

    Changed needle colors

--- sim/simulation_graphic.py
+++ sim/simulation_graphic.py
@@ -6,13 +6,13 @@ def create_image(path, result):
     scale = 100
     img = Image.new('RGB', (result.needle_length * 2 * result.line_count * scale, result.height * scale), color='white')
     d = ImageDraw.Draw(img)
-    line_color = (0, 0, 255)
+    line_color = (0, 0, 0)
     for x in range(result.line_count):
         d.line([((result.line_distance * x + result.line_distance / 2) * scale, 0),
                 ((result.line_distance * x + result.line_distance / 2) * scale, result.height * scale)],
                fill=line_color, width=10)
     needle_color_default = (255, 0, 0)
-    needle_color_hit = (0, 255, 0)
+    needle_color_hit = "#0090c6ff"
     for needle in result.needles:
         x = needle[0]
         y = needle[1]
